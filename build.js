a38d11caa52d02e7cbac3169ddca2b0332a7d8c7
sharisiri <37027404+sharisiri@users.noreply.github.com>
2019-11-16 17:55:41 +0530

    Update build.js

--- build.js
+++ build.js
@@ -9,45 +9,45 @@ const client_cred = 'NjU0ZDU3MTc0NmU3NDBiYzlkYmU5Yjk0YzM1ZTI4MDY6NTI2NWY1MWExZWI
 var plObjects = [];
 (async() => {
         try {
-        	var accessToken = await got.post('https://accounts.spotify.com/api/token', {form:true, body : {grant_type :"client_credentials"}, headers: {Authorization : 'Basic ' + client_cred} });
-        	accessToken = JSON.parse(accessToken.body).access_token;
-        	// console.log(accessToken);
+            var accessToken = await got.post('https://accounts.spotify.com/api/token', {form:true, body : {grant_type :"client_credentials"}, headers: {Authorization : 'Basic ' + client_cred} });
+            accessToken = JSON.parse(accessToken.body).access_token;
+            // console.log(accessToken);
             var playlists = await got('https://api.spotify.com/v1/users/ombudsmannen/playlists?limit=50', { json: true, headers: {Authorization : 'Bearer ' + accessToken} });
-    	  	var playlistItems = playlists.body.items;
-    	  	for (x in playlistItems) {
-    	  		if (playlistItems[x].name.startsWith('the goings on #')){
+            var playlistItems = playlists.body.items;
+            for (x in playlistItems) {
+                if (playlistItems[x].name.startsWith('the goings on #')){
                     // console.log(playlistItems[x].href);
-    	  			var pl = await got(playlistItems[x].href + '?market=SE', { json: true, headers: {Authorization : 'Bearer ' + accessToken} });
-    	  			var plObj = {};
-    	  			plObj.name = pl.body.name;
+                    var pl = await got(playlistItems[x].href + '?market=SE', { json: true, headers: {Authorization : 'Bearer ' + accessToken} });
+                    var plObj = {};
+                    plObj.name = pl.body.name;
                     plObj.number = plObj.name[plObj.name.indexOf('#') + 1];
-    	  			plObj.spotifyLink = pl.body.external_urls.spotify;
-    	  			plObj.image = 'img/' + plObj.name.replace(/\s/g, '-').replace('#','') + '.jpg';
+                    plObj.spotifyLink = pl.body.external_urls.spotify;
+                    plObj.image = 'img/' + plObj.name.replace(/\s/g, '-').replace('#','') + '.jpg';
                     await sharp(plObj.image,{progressive: true}).resize(960, 960).max().toFile('site/' + plObj.image);
-    	  			plObj.curator = (new RegExp(/curated by ([\s\S]*?)\./g)).exec(pl.body.description)[1];
-    	  			plObj.tracks = [];
-    	  			plObj.duration = 0;
+                    plObj.curator = (new RegExp(/curated by ([\s\S]*?)\./g)).exec(pl.body.description)[1];
+                    plObj.tracks = [];
+                    plObj.duration = 0;
                     var noPreviewTracks = {}
-    	  			var tracks = pl.body.tracks.items;
+                    var tracks = pl.body.tracks.items;
                     var ytTracks = '';
-    	  			for (y in tracks) {
-    	  				var track = {};
-    	  				_tr_num = parseInt(y) + 1;
-    	  				track.number = _tr_num;//(_tr_num < 10) ? '0' + _tr_num : '' + _tr_num;
-    	  				track.artist = tracks[y].track.artists.map(function(elem){return elem.name;}).join(", ");
-    	  				track.title = tracks[y].track.name;
+                    for (y in tracks) {
+                        var track = {};
+                        _tr_num = parseInt(y) + 1;
+                        track.number = _tr_num;//(_tr_num < 10) ? '0' + _tr_num : '' + _tr_num;
+                        track.artist = tracks[y].track.artists.map(function(elem){return elem.name;}).join(", ");
+                        track.title = tracks[y].track.name;
                         ytTracks += track.artist + ' - ' + track.title + '\n';
-    	  				track.duration = msToTime(tracks[y].track.duration_ms);
-    	  				plObj.duration += tracks[y].track.duration_ms;
-    	  				track.preview = tracks[y].track.preview_url;
+                        track.duration = msToTime(tracks[y].track.duration_ms);
+                        plObj.duration += tracks[y].track.duration_ms;
+                        track.preview = tracks[y].track.preview_url;
                         if (!track.preview) {
                              // console.log(track.artist + '-' + track.title + ' ' + track.preview + "\n\n");
                             noPreviewTracks[tracks[y].track.id] = y;
                         }
-    	  				track.spotifyLink = tracks[y].track.external_urls.spotify;
-    	  				track.youtubeLink = 'https://www.youtube.com/results?search_query=' + track.artist.replace(/\s/g,'+').replace('/\,/g','') + '+' + track.title.replace(/\s/g,'+');
-    	  				plObj.tracks.push(track);
-    	  			}
+                        track.spotifyLink = tracks[y].track.external_urls.spotify;
+                        track.youtubeLink = 'https://www.youtube.com/results?search_query=' + track.artist.replace(/\s/g,'+').replace('/\,/g','') + '+' + track.title.replace(/\s/g,'+');
+                        plObj.tracks.push(track);
+                    }
  
                     // noPrevKeys = Object.keys(noPreviewTracks);
                     // noPrevTrksData = await got('https://api.spotify.com/v1/tracks/?market=US&ids=' + noPrevKeys.join(','), { json: true, headers: {Authorization : 'Bearer ' + accessToken} });
@@ -67,19 +67,19 @@ var plObjects = [];
                     
                     plObjects.push(plObj);
 
-    	  		}
+                }
 
-    	  	}
-    	  	// console.log(plObjects);
+            }
+            // console.log(plObjects);
             plObjects.sort(compare);
-    	  	var _html = nunjucks.render('template.html', {playlists : plObjects});
-			var filePath = 'site/index.html';
-			fs.writeFile(filePath, _html, (err) => {  
-			    if (err) throw err;
-			    
-			});
+            var _html = nunjucks.render('template.html', {playlists : plObjects});
+            var filePath = 'site/index.html';
+            fs.writeFile(filePath, _html, (err) => {  
+                if (err) throw err;
+                
+            });
         } catch(error){
-        	console.log(error)
+            console.log(error)
         }
 })();
 
